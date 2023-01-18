40904f11817411190d7063ff726ecd3800afdfdc
Berk <70212751+BerkTas20@users.noreply.github.com>
2023-01-18 14:15:17 +0300

    updated

--- src/test/java/com/junit/test/TestApplicationTests.java
+++ src/test/java/com/junit/test/TestApplicationTests.java
@@ -42,13 +42,13 @@ class TestApplicationTests {
 
 	@org.junit.Test
 	public void getUsersTest() {
-		when(repository.findAll()).thenReturn(Stream.of(new User(101, "ashish", "user")).collect(Collectors.toList()));
+		when(repository.findAll()).thenReturn(Stream.of(new User(101, "berk", "user")).collect(Collectors.toList()));
 		assertEquals(1, service.getAllUsers().size());
 	}
 
 	@org.junit.Test
 	public void addUserTest() {
-		User user = new User(201, "bhagat", "superAdmin");
+		User user = new User(201, "berk", "superAdmin");
 
 		when(repository.save(user)).thenReturn(user);
 		assertEquals(user, service.saveUser(user));
@@ -56,7 +56,7 @@ class TestApplicationTests {
 
 	@org.junit.Test
 	public void deleteUser() {
-		User user = new User(201, "bhagat", "superAdmin");
+		User user = new User(201, "berk", "superAdmin");
 		service.deleteUSerById(user.getId());
 		verify(repository, times(1)).deleteById(user.getId());
 	}
